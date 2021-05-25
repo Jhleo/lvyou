9ce656d3bba1d0b885f436d491040b59b3108306
unknown <1370730262@qq.com>
2021-05-25 22:06:45 +0800

    first

---
+++ js/dataHotel.js
@@ -0,0 +1,196 @@
+const data1 = [{
+		name: "特威泽尔",
+		num: "101",
+		p:"1,149",
+		pic: "../img/hotel/hotel01.jpg"
+	},
+	{
+		name: "凯库拉",
+		num: "92",
+		p:"990",
+		pic: "../img/hotel/hotel02.jpg"
+	},
+	{
+		name: "瓦纳卡",
+		num: "352",
+		p:"1,276",
+		pic: "../img/hotel/hotel03.jpg"
+	},
+	{
+		name: "皇后镇",
+		num: "822",
+		p:"1,842",
+		pic: "../img/hotel/hotel04.jpg"
+	},
+	{
+		name: "特卡波湖",
+		num: "233",
+		p:"1,118",
+		pic: "../img/hotel/hotel05.jpg"
+	}
+]
+
+const data2 = [{
+		name: "酒店",
+		num: "698,157",
+		pic: "../img/hotel/hotel11.jpg"
+	},
+	{
+		name: "公寓",
+		num: "828,389",
+		pic: "../img/hotel/hotel12.jpg"
+	},
+	{
+		name: "度假村",
+		num: "21,922",
+		pic: "../img/hotel/hotel13.jpg"
+	},
+	{
+		name: "别墅",
+		num: "432,222",
+		pic: "../img/hotel/hotel14.jpg"
+	},
+	{
+		name: "木屋",
+		num: "14,387",
+		pic: "../img/hotel/hotel15.jpg"
+	},
+	{
+		name: "乡村别墅",
+		num: "127,922",
+		pic: "../img/hotel/hotel16.jpg"
+	},
+	{
+		name: "豪华帐篷",
+		num: "10,175",
+		pic: "../img/hotel/hotel17.jpg"
+	},
+	{
+		name: "公寓式酒店",
+		num: "35,400",
+		pic: "../img/hotel/hotel18.jpg"
+	},
+	{
+		name: "度假屋",
+		num: "432,222",
+		pic: "../img/hotel/hotel19.jpg"
+	},
+	{
+		name: "旅馆",
+		num: "137,382",
+		pic: "../img/hotel/hotel110.jpg"
+	},
+	{
+		name: "青旅",
+		num: "27,609",
+		pic: "../img/hotel/hotel111.jpg"
+	},
+	{
+		name: "汽车旅馆",
+		num: "15,880",
+		pic: "../img/hotel/hotel112.jpg"
+	},
+	{
+		name: "住宿加早餐旅馆",
+		num: "244,333",
+		pic: "../img/hotel/hotel113.jpg"
+	},
+	{
+		name: "日式旅馆",
+		num: "2,378",
+		pic: "../img/hotel/hotel114.jpg"
+	},
+	{
+		name: "庭院旅馆",
+		num: "1,315",
+		pic: "../img/hotel/hotel115.jpg"
+	},
+	{
+		name: "假日公园",
+		num: "6,866",
+		pic: "../img/hotel/hotel116.jpg"
+	},
+	{
+		name: "民宿",
+		num: "181,021",
+		pic: "../img/hotel/hotel117.jpg"
+	},
+	{
+		name: "露营地",
+		num: "8,778",
+		pic: "../img/hotel/hotel118.jpg"
+	},
+	{
+		name: "乡村民宿",
+		num: "15,337",
+		pic: "../img/hotel/hotel119.jpg"
+	},
+	{
+		name: "船屋",
+		num: "1,632",
+		pic: "../img/hotel/hotel120.jpg"
+	},
+	{
+		name: "农家乐",
+		num: "11,538",
+		pic: "../img/hotel/hotel121.jpg"
+	},
+	{
+		name: "豪华帐篷",
+		num: "3,023",
+		pic: "../img/hotel/hotel122.jpg"
+	},
+	{
+		name: "自助式住宿",
+		num: "670,643",
+		pic: "../img/hotel/hotel123.jpg"
+	},
+	{
+		name: "小屋",
+		num: "352",
+		pic: "../img/hotel/hotel124.jpg"
+	}
+]
+
+const data3 = [
+	{
+		name:"老城公寓酒店",
+		address:"克拉科夫",
+		p:"457",
+		fen:"9.1",
+		num:"3,585",
+		pic:"../img/hotel/hotel21.jpg"
+	},
+	{
+		name:"休格公寓式酒店",
+		address:"里约热内卢",
+		p:"238",
+		fen:"9.1",
+		num:"311",
+		pic:"../img/hotel/hotel22.jpg"
+	},
+	{
+		name:"汤普华6号老城公寓",
+		address:"布拉格",
+		p:"269",
+		fen:"9.2",
+		num:"121",
+		pic:"../img/hotel/hotel23.jpg"
+	},
+	{
+		name:"莱蒙洛克公寓式酒店",
+		address:"伦敦",
+		p:"674",
+		fen:"9.0",
+		num:"1,156",
+		pic:"../img/hotel/hotel24.jpg"
+	},
+	{
+		name:"M7现代公寓 - 我的家外之家",
+		address:"佛罗伦萨",
+		p:"699",
+		fen:"9.0",
+		num:"323",
+		pic:"../img/hotel/hotel26.jpg"
+	}
+]
---
+++ js/dataScenic.js
@@ -0,0 +1,176 @@
+const data1 = [{
+		theme: "登高踏青",
+		themepic:"../img/sce/sceheader01.jpg",
+		details: [{
+				name: "黄山",
+				info: "春意撩人 登高踏青赏春色",
+				pic: "../img/sce/sce01.jpg"
+			},
+			{
+				name: "峨眉山",
+				info: "阳春三月 闻着茶香去峨眉",
+				pic: "../img/sce/sce02.jpg"
+			},
+			{
+				name: "五台山",
+				info: "迎春 祈福 登五台",
+				pic: "../img/sce/sce03.jpg"
+			},
+			{
+				name: "庐山",
+				info: "亲近自然 春游庐山",
+				pic: "../img/sce/sce04.jpg"
+			}
+		]
+	},
+	{
+		theme: "草原撒欢",
+		themepic:"../img/sce/sceheader02.jpg",
+		details: [{
+				name: "锡林郭勒",
+				info: "草原 牧民 小马驹",
+				pic: "../img/sce/sce11.jpg"
+			},
+			{
+				name: "坝上",
+				info: "帝都最近的草原",
+				pic: "../img/sce/sce12.jpg"
+			},
+			{
+				name: "若尔盖",
+				info: "高原上的绿洲",
+				pic: "../img/sce/sce13.jpg"
+			},
+			{
+				name: "祁连山",
+				info: "风景如画 醉美祁连山",
+				pic: "../img/sce/sce14.jpg"
+			}
+		]
+	},
+	{
+		theme: "小镇时光",
+		themepic:"../img/sce/sceheader03.jpg",
+		details: [{
+				name: "西塘",
+				info: "烟雨长廊 景色好似宣纸画",
+				pic: "../img/sce/sce21.jpg"
+			},
+			{
+				name: "和顺",
+				info: "古风犹存 如诗如画",
+				pic: "../img/sce/sce22.jpg"
+			},
+			{
+				name: "乌镇",
+				info: "推窗见水 小桥人家",
+				pic: "../img/sce/sce23.jpg"
+			},
+			{
+				name: "平遥",
+				info: "晋商遗风 古城探幽",
+				pic: "../img/sce/sce24.jpg"
+			}
+		]
+	},
+	{
+		theme: "美食之城",
+		themepic:"../img/sce/sceheader04.jpg",
+		details: [{
+				name: "香港",
+				info: "只有想不到，没有找不到",
+				pic: "../img/sce/sce31.jpg"
+			},
+			{
+				name: "成都",
+				info: "不只有火锅，吃是头等事",
+				pic: "../img/sce/sce32.jpg"
+			},
+			{
+				name: "扬州",
+				info: "吴侬软语 淮味养人",
+				pic: "../img/sce/sce33.jpg"
+			},
+			{
+				name: "长沙",
+				info: "美食怕不辣，湘味最十足",
+				pic: "../img/sce/sce34.jpg"
+			}
+		]
+	},
+	{
+		theme: "文艺清新",
+		themepic:"../img/sce/sceheader05.jpg",
+		details: [{
+				name: "伦敦",
+				info: "风情英伦，帝国余韵",
+				pic: "../img/sce/sce41.jpg"
+			},
+			{
+				name: "清迈",
+				info: "泰北玫瑰，慢行古城",
+				pic: "../img/sce/sce42.jpg"
+			},
+			{
+				name: "济州岛",
+				info: "最传统的浪漫，最神秘的自然",
+				pic: "../img/sce/sce43.jpg"
+			},
+			{
+				name: "罗马",
+				info: "永恒之城，魅力无限",
+				pic: "../img/sce/sce44.jpg"
+			}
+		]
+	},
+	{
+		theme: "别样建筑",
+		themepic:"../img/sce/sceheader06.jpg",
+		details: [{
+				name: "巴塞罗那",
+				info: "天使之城，艺术之都",
+				pic: "../img/sce/sce51.jpg"
+			},
+			{
+				name: "米兰",
+				info: "在时尚与古典间穿梭",
+				pic: "../img/sce/sce52.jpg"
+			},
+			{
+				name: "莫斯科",
+				info: "畅饮伏特加的英雄之城",
+				pic: "../img/sce/sce53.jpg"
+			},
+			{
+				name: "伊斯坦布尔",
+				info: "古老与现代辉映，东方与西方交融",
+				pic: "../img/sce/sce54.jpg"
+			}
+		]
+	},
+	{
+		theme: "浪漫主义",
+		themepic:"../img/sce/sceheader07.jpg",
+		details: [{
+				name: "马尔代夫",
+				info: "一岛一世界",
+				pic: "../img/sce/sce61.jpg"
+			},
+			{
+				name: "圣托里尼",
+				info: "蓝白色的人间伊甸园",
+				pic: "../img/sce/sce62.jpg"
+			},
+			{
+				name: "巴黎",
+				info: "浪漫如斯，热情如是",
+				pic: "../img/sce/sce63.jpg"
+			},
+			{
+				name: "普罗旺斯",
+				info: "薰衣草之乡",
+				pic: "../img/sce/sce64.jpg"
+			}
+		]
+	}
+]
